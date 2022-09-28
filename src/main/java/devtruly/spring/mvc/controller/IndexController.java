67579ce83d70a73e2da29e9be214c250216e0ad9
dev-sincerity <kllliv50@gmail.com>
2022-09-28 15:08:22 +0900

    IndexController 셋팅

---
+++ src/main/java/devtruly/spring/mvc/controller/IndexController.java
@@ -0,0 +1,15 @@
+package devtruly.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping(path = {"/"})
+    public String getIndex() {
+        return "index";
+    }
+}
