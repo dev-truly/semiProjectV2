287e807107f4d9c44486dce4d3bb7fe2137115dc
dev-sincerity <kllliv50@gmail.com>
2022-09-29 15:35:31 +0900

    로그인 페이지 작성

--- src/main/java/devtruly/spring/mvc/controller/MemberController.java
+++ src/main/java/devtruly/spring/mvc/controller/MemberController.java
@@ -2,6 +2,7 @@ package devtruly.spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -10,9 +11,19 @@ public class MemberController {
         return "member/join";
     }
 
+    @PostMapping(path = {"/join"})
+    public String joinOk() {
+        return "redirect:login";
+    }
+
     @GetMapping(path = {"/login"})
     public String login() {
         return "member/login";
+    }
+
+    @PostMapping(path = {"/login"})
+    public String loginOk() {
+        return "redirect:/myinfo";
     }
 
     @GetMapping(path = {"/myinfo"})
